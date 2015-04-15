425f016d1dd45ce859d774a023f99b5b9be375b6
Fabian Buch <fabian.buch@dm.de>
2015-04-15 16:43:45 +0200

    don't show detail health checks as basic health checks
    don't inherit from BasicHealthIndicator but from HealthIndicator instead

--- src/main/java/de/filiadata/infrastructure/actuator/health/indicator/DetailHealthIndicator.java
+++ src/main/java/de/filiadata/infrastructure/actuator/health/indicator/DetailHealthIndicator.java
@@ -1,6 +1,7 @@
 package de.filiadata.infrastructure.actuator.health.indicator;
 
 import de.filiadata.infrastructure.actuator.health.endpoint.ExtendedHealthEndpoint;
+import org.springframework.boot.actuate.health.HealthIndicator;
 
 /**
  * Marker interface for detailed health indicators that should be available in /health/detail.
@@ -9,5 +10,5 @@ import de.filiadata.infrastructure.actuator.health.endpoint.ExtendedHealthEndpoi
  *
  * @see ExtendedHealthEndpoint
  */
-public interface DetailHealthIndicator extends BasicHealthIndicator {
+public interface DetailHealthIndicator extends HealthIndicator {
 }

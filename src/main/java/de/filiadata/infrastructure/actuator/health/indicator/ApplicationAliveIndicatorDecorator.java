4fea68360e7cbc2b9a2fc1d88ac3edaf2da667d8
Fels, Jakob <Jakob.Fels@dm.de>
2016-06-06 21:51:27 +0200

    Add decorators for all three extended interfaces so that spring boot HealthIndicators can be wrapped.

---
+++ src/main/java/de/filiadata/infrastructure/actuator/health/indicator/ApplicationAliveIndicatorDecorator.java
@@ -0,0 +1,25 @@
+package de.filiadata.infrastructure.actuator.health.indicator;
+
+import org.springframework.boot.actuate.health.CompositeHealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+
+/**
+ * Wraps a regular {@link HealthIndicator} to expose it as an {@link ApplicationAliveIndicator},
+ * e.g. to wrap a {@link CompositeHealthIndicator}.
+ *
+ * @author Jakob Fels
+ */
+public class ApplicationAliveIndicatorDecorator implements ApplicationAliveIndicator {
+
+    private final HealthIndicator delegate;
+
+    public ApplicationAliveIndicatorDecorator(HealthIndicator delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public Health health() {
+        return delegate.health();
+    }
+}

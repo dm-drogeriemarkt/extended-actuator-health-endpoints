267a1d98ce31e96f8158e52f152c045670e420dc
Fels, Jakob <Jakob.Fels@dm.de>
2016-10-24 08:12:19 +0200

    COUP-273: Fix minor sonar issues

--- src/main/java/de/filiadata/infrastructure/actuator/health/endpoint/AliveHealthEndpoint.java
+++ src/main/java/de/filiadata/infrastructure/actuator/health/endpoint/AliveHealthEndpoint.java
@@ -3,14 +3,12 @@ package de.filiadata.infrastructure.actuator.health.endpoint;
 import de.filiadata.infrastructure.actuator.health.indicator.ApplicationAliveIndicator;
 import org.springframework.boot.actuate.health.HealthAggregator;
 
-import java.util.Map;
-
 public class AliveHealthEndpoint extends ExtendedHealthEndpoint<ApplicationAliveIndicator> {
 
     /**
      * Create new AliveHealthEndpoint.
      *
-     * @param id part of the endpoint URL
+     * @param id               part of the endpoint URL
      * @param healthAggregator usually a new instance of OrderedHealthAggregator
      */
     public AliveHealthEndpoint(String id, HealthAggregator healthAggregator) {
--- src/main/java/de/filiadata/infrastructure/actuator/health/endpoint/DetailHealthEndpoint.java
+++ src/main/java/de/filiadata/infrastructure/actuator/health/endpoint/DetailHealthEndpoint.java
@@ -2,16 +2,13 @@ package de.filiadata.infrastructure.actuator.health.endpoint;
 
 import de.filiadata.infrastructure.actuator.health.indicator.DetailHealthIndicator;
 import org.springframework.boot.actuate.health.HealthAggregator;
-import org.springframework.boot.actuate.health.HealthIndicator;
-
-import java.util.Map;
 
 public class DetailHealthEndpoint extends ExtendedHealthEndpoint<DetailHealthIndicator> {
 
     /**
      * Create new DetailHealthEndpoint.
      *
-     * @param id part of the endpoint URL
+     * @param id               part of the endpoint URL
      * @param healthAggregator usually a new instance of OrderedHealthAggregator
      */
     public DetailHealthEndpoint(String id, HealthAggregator healthAggregator) {
